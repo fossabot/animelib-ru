ce8946d931f8677ad51812ff72c39a9af4acc5c8
Daniil Markov <mistminelol@gmail.com>
2020-08-20 11:53:38 +0300

    fixing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    mode: "production",
     entry: [
         './app/assets/js/main.js',
     ],
     output: {
         filename: './js/bundle.js'
-    },
-    devtool: "source-map",
-    module: {
-        rules: [{
-            test: /\.js$/,
-            include: path.resolve(__dirname, '/app/assets/js/main.js'),
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: 'env'
-                }
-            }
-        },
-        ]
     },
     plugins: [
     ]
