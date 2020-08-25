7a5609373698fcb3358b5cc6e9d704903ef6e8b7
Daniil Markov <mistminelol@gmail.com>
2020-08-25 12:36:59 +0300

    microfix

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,8 +1,6 @@
-import jQuery from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
+// import jQuery from 'jquery';
+// import popper from 'popper.js';
+// import bootstrap from 'bootstrap';
 import Turbolinks from 'turbolinks';
 
 Turbolinks.start();
-
-console.log('2');
